c9aa831afe6ac9d454334606c34b1986819fc405
Jerinislam05 <jerinislam05@gmail.com>
2023-10-11 00:37:46 +0100

    learn about push-pop

---
+++ push-pop.js
@@ -0,0 +1,15 @@
+var numbers = [11, 12, 13, 14, 55]
+// kono array te jodi kono number ba new element add korte hoy last upadan hisebe tahole push korte hobe
+// numbers.push(60)
+console.log(numbers); 
+var friends = ['balam', 'kalam', 'salam'];
+friends.push('gelam');
+console.log(friends);
+
+// last er element ber korte hole pop use korte hobe
+// numbers.pop();
+console.log(numbers);
+// pop kora element ber korte pop k ekta variable er moddhe rakhte hobe
+var element = numbers.pop();
+console.log(numbers);
+console.log(element);
