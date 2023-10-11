10676ab310e01f042455f3c7e9f3d4198c7f1e39
Jerinislam05 <jerinislam05@gmail.com>
2023-10-12 00:57:33 +0100

    learn multiple-conditions

---
+++ conditional.js
@@ -0,0 +1,20 @@
+var iphonePrice = 80000;
+var myBudget = 100000;
+
+if (iphonePrice < myBudget) {
+    console.log('buy iphone');
+}
+var chickenPrice = 500;
+var myBudget = 50;
+if (chickenPrice < myBudget) {
+    console.log('buy chicken');
+}
+if (chickenPrice > myBudget) {
+    console.log('smashed potato');
+}
+if (chickenPrice < myBudget) {
+    console.log('chicken');
+}
+else{
+    console.log('potato');
+}
---
+++ multiple-condition.js
@@ -0,0 +1,39 @@
+// var isGraduated = true;
+// var salary = 75000;
+// var cars = 0;
+
+// if(isGraduated == true){
+//     console.log('biye korbo');
+// }
+// else{
+//     console.log('biye nai');
+// }
+
+// if (isGraduated == true && salary > 50000) {
+//     console.log('biye korbo');
+// }
+// else{
+//     console.log('biye nai');
+// }
+// if (isGraduated == true && salary > 50000 && cars > ) {
+//     console.log('biye korbo');
+// }
+// else{
+//     console.log('biye nai');
+// }
+
+var isGraduated = false;
+var salary = 51000;
+var cars = 1;
+
+// if (isGraduated === true || salary > 50000 || cars >= 1) {
+//     console.log('prem kori');
+// }else{
+//     console.log('number dao');
+// }
+
+if (isGraduated === true && salary > 50000 || cars >= 1) {
+    console.log('lets go');
+}else{
+    console.log('no need');
+}
