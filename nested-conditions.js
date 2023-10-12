d40e4cfad9190682420102132c25a8ec94b19545
Jerinislam05 <jerinislam05@gmail.com>
2023-10-12 20:38:32 +0100

    nested condition

---
+++ nested-conditions.js
@@ -0,0 +1,21 @@
+var math = true;
+var geometry = true;
+var straightLine = false;
+
+if(math == true){
+    if (geometry == true) {
+        if (straightLine == true) {
+            
+        }
+        else {
+            console.log('baka poth valo na');
+        }
+        
+    }
+    else {
+        console.log('not good');
+    }
+}
+else {
+
+}
