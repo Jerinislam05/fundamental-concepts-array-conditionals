80e9ab300ced113e030443ea8c5d7d7872249744
Jerinislam05 <jerinislam05@gmail.com>
2023-10-12 21:02:49 +0100

    two more comparison

---
+++ summary.js
@@ -0,0 +1,24 @@
+var destination = ['sajek', 'coxbazar', 'bandorban', 'sundarbon', 'bagerhat']; //index er man suru hobe 0 diye
+
+console.log(destination[2]); //array er value read korar niyom
+destination[2] = 'thailand'; // index er man set korte
+var position = destination.indexOf('bagerhat') //index er position number jante indexOf use hoy
+
+destination.push('singapur'); //add korte push
+destination.pop(); //remove korte pop
+
+// conditionals
+// <, >, ==, !=, <=, >=, &&, ||
+
+if (destination[1] === 'sajek') {
+    
+}
+else if (destination[2] === 'bali') {
+    
+}
+else if (destination.length !== 3) {
+
+}
+else {
+    
+}
