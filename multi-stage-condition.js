4788bedbf3cb458b071322e9bd04ee44e361a09b
Jerinislam05 <jerinislam05@gmail.com>
2023-10-12 20:26:04 +0100

    learn multi-stage-condition

---
+++ multi-stage-condition.js
@@ -0,0 +1,17 @@
+var money = 42;
+var danishPrice = 40;
+var butterBread = 30;
+var toastBiscuit = 20;
+
+if (danishPrice < money) {
+    console.log('danish khabo');
+}
+else if (butterBread < money) {
+    console.log('butterBread khabo');
+}
+else if (toastBiscuit < money) {
+    console.log('toastBiscuit khabo');
+}
+else {
+    console.log('only cha');
+}
