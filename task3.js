831a047fd1750de044b877d3694151dcfadef857
Jerinislam05 <jerinislam05@gmail.com>
2023-10-15 12:38:28 +0100

    if else learning

--- task3.js
+++ task3.js
@@ -1,4 +1,19 @@
-var number = [13, 79, 45];
+var num1 = 13;
+var num2 = 79;
+var num3 = 45;
 
-var largest = Math.max(...number);
-console.log(largest);
+if (num1 > num2) {
+    // console.log('runing test');
+  if (num1 > num3) {
+    console.log('num1 is largest');
+  } else if (num3 > num1) {
+    console.log('num3 is largest');
+  }
+} else if (num1 < num2) {
+    // console.log('runing test');
+  if (num2 > num3) {
+    console.log('num2 is largest');
+  } else if (num3 > num2) {
+    console.log('num3 is largest');
+  }
+}
